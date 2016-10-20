6f395ff8130a7d622c07583bf78b719fb60108fb
ligadelagua <ligadelagua@gmail.com>
2016-10-20 14:03:49 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << Rails.root.join("app", "assets")
-    config.assets.compile = false
     
   end
 end
