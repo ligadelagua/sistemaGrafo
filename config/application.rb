e2bfb75155917f5534134b86f506b2b308a8f67a
ligadelagua <ligadelagua@gmail.com>
2016-10-20 14:07:40 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module Workspace
      config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << File.join(Rails.root, '/app/assets')
-    
+    config.active_record.raise_in_transactional_callbacks = true    
   end
 end
