1b147357eef77eed547f010a892548766b3557a5
wzappepexd <dmedinac@uelbosque.edu.co>
2016-11-09 12:24:48 +0000

    Iconos url imagen

--- app/assets/javascripts/js/nodos.js
+++ app/assets/javascripts/js/nodos.js
@@ -10,37 +10,69 @@ var nombre = Array.prototype.slice.call(document.getElementsByClassName('nombre'
 var descripO = Array.prototype.slice.call(document.getElementsByClassName('descripO'));
 var logo = Array.prototype.slice.call(document.getElementsByClassName('logo'));
 var banner = Array.prototype.slice.call(document.getElementsByClassName('banner'));
-var organizaciones = nombre.map(function(nombre) { return nombre.value; });
-var descripcionesO = descripO.map(function(descripO) { return descripO.value; });
-var logoO = logo.map(function(logo) { return logo.value; });
-var bannerO = banner.map(function(banner) { return banner.value; });
+var organizaciones = nombre.map(function(nombre) {
+  return nombre.value;
+});
+var descripcionesO = descripO.map(function(descripO) {
+  return descripO.value;
+});
+var logoO = logo.map(function(logo) {
+  return logo.value;
+});
+var bannerO = banner.map(function(banner) {
+  return banner.value;
+});
 
 var org1 = Array.prototype.slice.call(document.getElementsByClassName('org1'));
 var org2 = Array.prototype.slice.call(document.getElementsByClassName('org2'));
 var descripC = Array.prototype.slice.call(document.getElementsByClassName('descripC'));
 var bannerC = Array.prototype.slice.call(document.getElementsByClassName('bannerC'));
-var orgs1 = org1.map(function(org1) { return org1.value; });
-var orgs2 = org2.map(function(org2) { return org2.value; });
-var descripcionesC = descripC.map(function(descripC) { return descripC.value; });
-var bannersC = bannerC.map(function(bannerC) { return bannerC.value; });
+var orgs1 = org1.map(function(org1) {
+  return org1.value;
+});
+var orgs2 = org2.map(function(org2) {
+  return org2.value;
+});
+var descripcionesC = descripC.map(function(descripC) {
+  return descripC.value;
+});
+var bannersC = bannerC.map(function(bannerC) {
+  return bannerC.value;
+});
 
 var nomC = Array.prototype.slice.call(document.getElementsByClassName('nomC'));
 var tituloC = Array.prototype.slice.call(document.getElementsByClassName('tituloC'));
 var urlC = Array.prototype.slice.call(document.getElementsByClassName('urlC'));
 var uploadC = Array.prototype.slice.call(document.getElementsByClassName('uploadC'));
-var nomsC = nomC.map(function(nomC) { return nomC.value; });
-var titulosC = tituloC.map(function(tituloC) { return tituloC.value; });
-var urlsC = urlC.map(function(urlC) { return urlC.value; });
-var uploadsC = uploadC.map(function(uploadC) { return uploadC.value; });
+var nomsC = nomC.map(function(nomC) {
+  return nomC.value;
+});
+var titulosC = tituloC.map(function(tituloC) {
+  return tituloC.value;
+});
+var urlsC = urlC.map(function(urlC) {
+  return urlC.value;
+});
+var uploadsC = uploadC.map(function(uploadC) {
+  return uploadC.value;
+});
 
 var nomO = Array.prototype.slice.call(document.getElementsByClassName('nomO'));
 var tituloO = Array.prototype.slice.call(document.getElementsByClassName('tituloO'));
 var urlO = Array.prototype.slice.call(document.getElementsByClassName('urlO'));
 var uploadO = Array.prototype.slice.call(document.getElementsByClassName('uploadO'));
-var nomsO = nomO.map(function(nomO) { return nomO.value; });
-var titulosO = tituloO.map(function(tituloO) { return tituloO.value; });
-var urlsO = urlO.map(function(urlO) { return urlO.value; });
-var uploadsO = uploadO.map(function(uploadO) { return uploadO.value; });
+var nomsO = nomO.map(function(nomO) {
+  return nomO.value;
+});
+var titulosO = tituloO.map(function(tituloO) {
+  return tituloO.value;
+});
+var urlsO = urlO.map(function(urlO) {
+  return urlO.value;
+});
+var uploadsO = uploadO.map(function(uploadO) {
+  return uploadO.value;
+});
 
 // InfoCard - Animación de entrada
 var infoCard = $('.node-info');
@@ -63,41 +95,42 @@ function showInfoCard(params) {
   var pArco = params.edges[0];
   var pos;
   var newHTML = [];
-   
+
   infoCard.removeClass('hidden fadeOutDown');
   infoCard.addClass('fadeInUp');
-  
-  if (pNodo === undefined){
-    orgDescripcion.html(orgs1[pArco] + " y " + orgs2[pArco]);  
+
+  if (pNodo === undefined) {
+    orgDescripcion.html(orgs1[pArco] + " y " + orgs2[pArco]);
     descripcion.html(descripcionesC[pArco]);
     orgRecurso.html(orgs1[pArco] + " y " + orgs2[pArco]);
     document.getElementById("logos").src = bannersC[pArco];
     newHTML.push('<br/><br/><br/>');
-    for (pos=0;pos<(nomsC.length);pos++){ 
-      if((orgs1[pArco] + " + " + orgs2[pArco]) == nomsC[pos]){
-        if(urlsC[pos] != ""){
-          newHTML.push('<a href=' + urlsC[pos] + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
-        } 
-        if(uploadC[pos].defaultValue != "missing.png"){
-          newHTML.push('<a href=' + uploadC[pos].defaultValue + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
+    for (pos = 0; pos < (nomsC.length); pos++) {
+      if ((orgs1[pArco] + " + " + orgs2[pArco]) == nomsC[pos]) {
+        if (urlsC[pos] != "") {  
+          newHTML.push('<h3 class="icon fa-picture-o"></h3>' + '<a href=' + urlsC[pos] + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
+        }
+        if (uploadC[pos].defaultValue != "missing.png") {
+          newHTML.push('<h3 class="icon fa-external-link"></h3>' + '<a href=' + uploadC[pos].defaultValue + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
         }
       }
       recursos.html(newHTML.join(""));
     }
-    
-  }else{
-    orgDescripcion.html(organizaciones[pNodo]);  
+
+  }
+  else {
+    orgDescripcion.html(organizaciones[pNodo]);
     descripcion.html(descripcionesO[pNodo]);
     orgRecurso.html(organizaciones[pNodo]);
     document.getElementById("logos").src = bannerO[pNodo];
     newHTML.push('<br/><br/><br/>');
-    for (pos=0;pos<(nomsO.length);pos++){ 
-      if((organizaciones[pNodo]) == nomsO[pos]){
-        if(urlsO[pos] != ""){
-          newHTML.push('<a href=' + urlsO[pos] + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
-        } 
-        if(uploadO[pos].defaultValue != "missing.png"){
-          newHTML.push('<a href=' + uploadO[pos].defaultValue + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
+    for (pos = 0; pos < (nomsO.length); pos++) {
+      if ((organizaciones[pNodo]) == nomsO[pos]) {
+        if (urlsO[pos] != "") {
+          newHTML.push('<h3 class="icon fa-picture-o"></h3>' +'<a href=' + urlsO[pos] + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
+        }
+        if (uploadO[pos].defaultValue != "missing.png") {
+          newHTML.push('<h3 class="icon fa-external-link"></h3>' + '<a href=' + uploadO[pos].defaultValue + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
         }
       }
       recursos.html(newHTML.join(""));
@@ -107,7 +140,7 @@ function showInfoCard(params) {
 
 function hideInfoCard() {
   infoCard.removeClass('hidden fadeInUp');
-  infoCard.addClass('fadeOutDown');  
+  infoCard.addClass('fadeOutDown');
   nodeNumber.html('');
 }
 
@@ -115,38 +148,53 @@ function getScaleFreeNetwork(nodeCount) {
   var DIR = 'img/';
   var pos;
   options = {
-  height: '100%',
-  width: '100%',
-  interaction: {
-    tooltipDelay: 500,
-    hover: false
-  },
-  nodes: {
-          borderWidth:4,
-          size:50,
-        color: {
-            border: '#222222',
-            background: '#666666'
-          },
-          font:{color:'#eeeeee'}
-        },
-        edges: {
-          color: 'lightgray'
-        }
-};
-  if(nodes == null)
+    height: '100%',
+    width: '100%',
+    interaction: {
+      tooltipDelay: 100,
+      hover: false
+    },
+    nodes: {
+      borderWidth: 4,
+      size: 30,
+      color: {
+        border: '#222222',
+        background: '#666666'
+      },
+      font: {
+        color: '#eeeeee'
+      },
+    },
+    edges: {
+      color: 'lightgray'
+    }
+  };
+  if (nodes == null)
     nodes = [];
-  for (pos=0;pos<(organizaciones.length);pos++){ 
-   	nodes.push( {id: pos,  shape: 'circularImage', image:logoO[pos] , label:organizaciones[pos] });
+  for (pos = 0; pos < (organizaciones.length); pos++) {
+    nodes.push({
+      id: pos,
+      shape: 'circularImage',
+      image: logoO[pos],
+      label: organizaciones[pos]
+    });
   }
 
-  if(edges == null)
+  if (edges == null)
     edges = [];
-  for (pos=0;pos<(orgs1.length);pos++){ 
-    var o1 = nodes.map(function(x) {return x.label; }).indexOf(orgs1[pos]);
-    var o2 = nodes.map(function(x) {return x.label; }).indexOf(orgs2[pos]);
-   	edges.push( {id: pos,from: o1, to: o2} );
-   	
+  for (pos = 0; pos < (orgs1.length); pos++) {
+    var o1 = nodes.map(function(x) {
+      return x.label;
+    }).indexOf(orgs1[pos]);
+    var o2 = nodes.map(function(x) {
+      return x.label;
+    }).indexOf(orgs2[pos]);
+    edges.push({
+      id: pos,
+      from: o1,
+      to: o2
+    });
+
   }
 
   return {
--- public/assets/js/nodos.js
+++ public/assets/js/nodos.js
@@ -107,11 +107,11 @@ function showInfoCard(params) {
     newHTML.push('<br/><br/><br/>');
     for (pos = 0; pos < (nomsC.length); pos++) {
       if ((orgs1[pArco] + " + " + orgs2[pArco]) == nomsC[pos]) {
-        if (urlsC[pos] != "") {
-          newHTML.push('<a href=' + urlsC[pos] + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
+        if (urlsC[pos] != "") {  
+          newHTML.push('<h3 class="icon fa-picture-o"></h3>' + '<a href=' + urlsC[pos] + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
         }
         if (uploadC[pos].defaultValue != "missing.png") {
-          newHTML.push('<a href=' + uploadC[pos].defaultValue + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
+          newHTML.push('<h3 class="icon fa-external-link"></h3>' + '<a href=' + uploadC[pos].defaultValue + ' target="_new">' + titulosC[pos] + '</a>' + '<br>');
         }
       }
       recursos.html(newHTML.join(""));
@@ -127,10 +127,10 @@ function showInfoCard(params) {
     for (pos = 0; pos < (nomsO.length); pos++) {
       if ((organizaciones[pNodo]) == nomsO[pos]) {
         if (urlsO[pos] != "") {
-          newHTML.push('<a href=' + urlsO[pos] + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
+          newHTML.push('<h3 class="icon fa-picture-o"></h3>' +'<a href=' + urlsO[pos] + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
         }
         if (uploadO[pos].defaultValue != "missing.png") {
-          newHTML.push('<a href=' + uploadO[pos].defaultValue + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
+          newHTML.push('<h3 class="icon fa-external-link"></h3>' + '<a href=' + uploadO[pos].defaultValue + ' target="_new">' + titulosO[pos] + '</a>' + '<br>');
         }
       }
       recursos.html(newHTML.join(""));
